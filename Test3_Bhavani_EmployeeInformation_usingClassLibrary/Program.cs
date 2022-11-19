ce0a4c6bc1432b93a3d4814684527e911386fa9d
adurga-ariqt <adurga@ariqt.com>
2022-11-19 14:01:56 +0530

    Add project files.

---
+++ Test3_Bhavani_EmployeeInformation_usingClassLibrary/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MyClassLibrary_Test2;
+
+Console.WriteLine("Hello, World!");
+Salary objSalary = new Salary();
+objSalary.CalculateSalary();
+Console.ReadLine();
